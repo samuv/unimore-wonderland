69818b93f5f2ad51868bdd0aa8de6e24ad3bd4f3
samuv <4377202+samuv@users.noreply.github.com>
2018-12-04 01:17:15 +0100

    change logo, change text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './unimo-logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edita <code>src/App.js</code> e salva per aggiornare la pagina.
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            More on React
           </a>
         </header>
       </div>
